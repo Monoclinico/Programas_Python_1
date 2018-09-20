f0eedad26ce813e1aaef037e0f5f349de3f94ff6
Matheus Sanches <matheussanchesleme@gmail.com>
2018-09-20 19:24:04 -0300

    Analisa a % de letras no texto.

---
+++ letras_no_texto.py
@@ -0,0 +1,39 @@
+arquivo = open('textos/texto_para_analisar.txt')
+texto = arquivo.read()
+arquivo.close()
+
+arquivo = open('textos/texto_para_analisar.txt')
+texto_original = arquivo.read()
+arquivo.close()
+
+texto = texto.strip()
+texto = texto.split()
+texto = ''.join(texto)
+texto = texto.lower()
+
+lista_exclusiva = ['á','à','é','ó','í','ã','õ','ç','ú','ê','â','ô','è']
+lista_substitut = ['a','a','e','o','i','a','o','c','u','e','a','o','e']
+
+for x in range(len(lista_exclusiva)):
+    k = texto.count(lista_exclusiva[x])
+    for c in range(k):
+        texto = texto.replace(lista_exclusiva[x], lista_substitut[x])
+
+lista_abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+             'n','o','p','q','r','s','t','u','v','w','x','y','z',
+             '0','1','2','3','4','5','6','7','8','9','?','!','"',
+             "'",'.',',','-','_','/','*','+','=','$','@','#','%',
+             '&','(',')','[',']','{','}',';',':','>','<']
+
+texto_p = ''
+for x in range(len(lista_abc)):
+    n = texto.count(lista_abc[x])
+    p = (n / len(texto)) * 100
+    if p > 0:
+#        print('({}) = {:.2f}%'.format(lista_abc[x], p))
+        texto_p += '\n({}) = {:.2f}%'.format(lista_abc[x], p)
+
+arquivo2 = open('textos/char_porcentagem.txt','w')
+arquivo2.write(texto_original + texto_p)
+arquivo2.close()
+
