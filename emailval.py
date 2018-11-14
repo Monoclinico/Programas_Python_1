1156a879e0c74cdd9849a56eda70176794463ecf
Matheus Sanches <matheussanchesleme@gmail.com>
2018-11-14 18:46:20 -0200

    Melhoramento e nova versão: V1.1

--- emailval.py
+++ emailval.py
@@ -1,4 +1,6 @@
-#Validador de e-mail 1.0
+#Função Validadora de formato de e-mail V1.1
+#+Função que divide o email e cria uma lista
+#Sem usar Regex
 
 def validae(email):
     """Verifica se um e-mail é válido, se for retorna True, se não retorna False"""
@@ -6,33 +8,31 @@ def validae(email):
     car_espec = ['á', 'à', 'é', 'ó', 'í', 'ã', 'õ', 'ç', 'ú', 'ê', 'â', 'ô',
                  'è', '+', '-', '/', '*', ':', '?', '!', ';', ',', 'ª', 'º',
                  '°', '>', '<', '=', '"', "'", '#', '&', '(', ')',
-                 '¬', '\\', '|', '^', '~', '´', '`', '..']
-    email1 = str(email).strip()
+                 '¬', '\\', '|', '^', '~', '´', '`', '..',' ']
+    email1 = str(email).strip()  #remove os espaços antes e depois da string
     especial =  False
     resposta = True
-    for c_a_r in car_espec:
+    for c_a_r in car_espec:  #verifica se algum símbolo da lista 'car_espec' está na string 'email1' 
         if c_a_r in email1:
             especial = True
-    if especial:
+    if especial:  #falseia 'resposta' se 'especial' for verdadeiro 
         resposta = False
-    if ' ' in email1:
-        resposta = False
-    if '@' in email1:
-        endereco = email1.split('@')
-        if '.' in endereco[1]:
-            endereco2 = endereco[1].split('.')
-            if '' in endereco2:
+    if '@' in email1: #verifica se o símbolo '@' está em 'email1'
+        endereco = email1.split('@') #reparte a string 'email1' onde está '@' criando uma lista X
+        if '.' in endereco[1]: #verifica se o símbolo '.' está na segunda string da lista X
+            endereco2 = endereco[1].split('.') #reparte a segunda string da lista X onde está '.' criando uma lista Y
+            if '' in endereco2: #verifica se a lista Y está com letras
                 resposta = False
         else:
             resposta = False
     else:
         resposta = False
-    return  resposta
+    return  resposta #retorna um valor Booleano
 
-def npt(email):
+def npt(email): #divide o email em três partes, como: contato.servicos@hotmail.com ==> 'contato.servicos', 'hotmail', 'com'
     if validae(email):
         lista = email.split('@')
         lista = [lista[0]] + lista[1].split('.')
-        return lista
+        return lista #retorna uma lista do email dividido se validae('email qualquer') == True
     else:
-        return 'Invalid E-mail'
+        return 'Invalid E-mail' #retorna 'Invalid E-mail' se validae('email qualquer') == False
