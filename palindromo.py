75ed70afa59c38f9bcd6740135e4437e78ed1ec2
Matheus Sanches <matheussanchesleme@gmail.com>
2019-06-16 13:41:10 -0300

    simplificação

--- palindromo.py
+++ palindromo.py
@@ -1,11 +1,9 @@
 #-*- coding: utf-8 -*-
 
 print('Teste de palíndromo')
-
-texto = str(input('Digite um texto: ')).strip().split() #entrada de dados (strings), remove os espaços
-
-texto = ''.join(texto) # junta tudo, assim ['m','u','n','d','o'] ==> ['mundo']
-
-if texto == texto[::-1]: print('Esse texto é palíndromo!') # testa se a string X é igual a string X inversa
-
-else: print('Esse texto não é palíndromo!')
+texto = str(input('Digite um texto: '))#entrada de dados (strings)
+texto = texto.replace(" ","")#remove todos os espaços
+if texto == texto[::-1]: 
+  print('Esse texto é palíndromo!') # testa se a string X é igual a string X inversa
+else: 
+  print('Esse texto não é palíndromo!')
