24400acb858ff6cb8aa76c0a63a1eebb7276f9ac
Matheus Sanches <matheussanchesleme@gmail.com>
2019-10-11 19:04:35 -0300

    correção de variáveis tempo e minutos

--- download_tempo.py
+++ download_tempo.py
@@ -4,16 +4,14 @@ def leitura():
   while True:
     try:
       print(('Qual o tamanho do arquivo(MB)?'))
-      t = float(input('->'))
+      tamanho = float(input('->'))
       print('Qual a velocidade da internet(Mbps)')
-      v = float(input('->'))
+      velocidade = float(input('->'))
     except:continue
     else:break
-  bytes_ps = v / 8
-  tempo = t / bytes_ps
-  seg = tempo % 60
-  minu = (tempo - seg) // 60
-  print(tempo)
-  print(f'Tempo: {minu:n}min e {seg:.3f}s')
+  tempo = (tamanho*8.0) / velocidade
+  minutos = tempo // 60
+  segundos_restantes = tempo % 60
+  print(f'Tempo: {minutos:n}min e {segundos_restantes:.3f}s')
 
 leitura()
