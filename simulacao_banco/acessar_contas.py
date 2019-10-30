16c0f4a51172dcadfc0d9d2690540422226e276f
Matheus Sanches <matheussanchesleme@gmail.com>
2019-10-30 18:13:58 -0300

    Foram acrescentadas parte das funções de acesso ao banco de dados e interfaces para interação do usuário.

---
+++ simulacao_banco/acessar_contas.py
@@ -0,0 +1,99 @@
+
+import os
+import sqlite3
+
+"""Interface para interagir com o banco de dados"""
+
+#funcao que define o caminho para o arquivo dos dados dos usuarios
+def acesso_tabela_contas(nome_arquivo,nome_diretorio):
+  try:
+    arquivo = nome_arquivo.strip()
+    pasta_projeto = nome_diretorio.strip()
+    caminho_abs = os.getcwd()
+    lista_arquivos = os.listdir(caminho_abs)
+    if (arquivo in lista_arquivos):
+      caminho_abs = os.path.abspath(arquivo)
+      caminho_abs = os.path.normcase(caminho_abs)
+      caminho_abs = os.path.normpath(caminho_abs)
+      return caminho_abs
+    else:
+      if  pasta_projeto in lista_arquivos:
+        caminho_abs += '\\' + pasta_projeto + '\\' + arquivo
+        caminho_abs = os.path.normcase(caminho_abs)
+        caminho_abs = os.path.normpath(caminho_abs)
+        return caminho_abs
+      else:
+        raise FileNotFoundError
+  except:
+    return 0
+
+
+#funcao que retorna os dados da conta e se a conta existe
+def retornar_conta(conta,senha):
+  conta_e_existencia = None
+  conta_pessoa = str(conta).strip()
+  senha_pessoa = str(senha)
+
+  conexao = sqlite3.connect(acesso_tabela_contas("contas_bancarias.db","simulacao_banco"))
+  navegar_manipular = conexao.cursor()
+  navegar_manipular.execute("""SELECT conta, nome, senha, saldo FROM clientes WHERE conta = (?) AND senha = (?)""",(conta_pessoa, senha_pessoa))
+  
+  conta_e_existencia = navegar_manipular.fetchall()
+  conexao.close()
+  return conta_e_existencia
+
+#funcao que cria a conta bancaria
+def criar_conta(nome,senha):
+  
+  nome_pessoa = str(nome).strip()
+  senha_pessoa = str(senha)
+  saldo_pessoa = 0
+  conta_pessoa = None
+
+  conexao = sqlite3.connect(acesso_tabela_contas("contas_bancarias.db","simulacao_banco"))
+  navegar_manipular = conexao.cursor()
+  navegar_manipular.execute("""SELECT conta FROM clientes ORDER BY conta DESC LIMIT 1""")
+  ultima_conta = navegar_manipular.fetchall()
+  if (len(ultima_conta)) > 0:
+    conta_pessoa = str(int(ultima_conta[0][0]) + 1)
+  else:
+    conta_pessoa = "1000"
+  conta = (conta_pessoa,nome_pessoa,senha_pessoa,saldo_pessoa)
+  navegar_manipular.execute("""INSERT INTO clientes (conta, nome, senha, saldo) VALUES (?,?,?,?)""",conta)
+  conexao.commit()
+  navegar_manipular.execute("""SELECT conta, nome, senha FROM clientes WHERE conta = (?) AND senha = (?)""",(conta_pessoa, senha_pessoa))
+  dados_conta = navegar_manipular.fetchall()
+  conexao.close()
+  return dados_conta
+    
+
+#def modificar_conta(conta,nome,senha):
+
+
+#funcao para criar a tabela para a base de dados
+def criar_tabela(criar=True):
+
+  conexao = sqlite3.connect(acesso_tabela_contas("contas_bancarias.db","simulacao_banco"))
+  navegar_manipular = conexao.cursor()
+  if (criar):
+    navegar_manipular.execute("""
+    CREATE TABLE clientes (
+      id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+      conta TEXT NOT NULL UNIQUE,
+      nome TEXT NOT NULL,
+      senha TEXT NOT NULL,
+      saldo REAL NOT NULL 
+
+    );
+    """)
+  conexao.close()
+
+#funcao para remover a tabela da base de dados
+def remover_tabela(remover=True):
+  conexao = sqlite3.connect(acesso_tabela_contas("contas_bancarias.db","simulacao_banco"))
+  navegar_manipular = conexao.cursor()
+  if (remover):
+    navegar_manipular.execute("""DROP TABLE clientes;""")
+  conexao.close()
+
+
--- simulacao_banco/simulacao_caixa_eletronico.py
+++ simulacao_banco/simulacao_caixa_eletronico.py
@@ -1,105 +1,252 @@
 from time import sleep
 import datetime
+import acessar_contas
 
-Total = 50000
-while True:
-    if Total < 5:
-        print('\nAcabaram as cédulas desta máquina')
-        break
-    while True:
-        print('\n' + 'BANCO PYTHON')
-        while True:
-            senha = str(input('Digite a senha de 4 dígitos: ')).strip()
-            if len(senha) == 4:
-                break
-            print('Senha incorreta')
-        valor = str(input('\nMínimo: R$ 5,00.\nValor para sacar(Não é possível sacar moedas): R$ ')).strip()
+def nome_banco():
+  print()
+  print("="*15+"BANCO PYTHON"+"="*15)
+
+def linha_estetica():
+  print()
+  print("="*30)
+
+def encerrar(sessao="SESSAO"):
+  print()
+  print("="*15+sessao+" ENCERRADA"+"="*15)
+
+def opcoes_conta():
+  print()
+  print("CRIAR CONTA [0] ================= [1] ACESSAR CONTA")
+  print("       SAIR [2] ================= ")
+  print()
+def opcoes_acesso(numero_conta, nome_pessoa):
+  print()
+  print("NOME: "+nome_pessoa)
+  print("CONTA: "+ numero_conta)
+  print("MOSTRAR SALDO [0] =============== [1] SACAR")
+  print("    DEPOSITAR [2] =============== [3] TRANSFERIR PARA OUTRA CONTA")
+  print("       VOLTAR [4] =============== [5] SAIR")
+  print()
+
+def conta_criada_com_sucesso():
+  print()
+  print("=========CONTA CRIADA COM SUCESSO=========")
+
+def acessar_caixa():
+  Total = 50000
+  while True:
+      if Total < 5:
+          print('\nAcabaram as cédulas desta máquina')
+          break
+      while True:
+          print('\n' + 'BANCO PYTHON')
+          while True:
+              senha = str(input('Digite a senha de 8 dígitos: ')).strip()
+              if len(senha) == 8:
+                  break
+              print('Senha incorreta')
+          valor = str(input('\nMínimo: R$ 5,00.\nValor para sacar(Não é possível sacar moedas): R$ ')).strip()
 
 
-        if valor.count(',') >= 1:
-            print('Digite o valor sem vírgula ou ponto, por favor.')
-        elif valor.count('.') >= 1:
-            print('Digite o valor sem vírgula ou ponto, por favor.')
-        elif int(valor) < 5:
-            print('O mínimo para saque é R$ 5,00.')
-        else:
-            if int(valor) <= Total:
-                print('Este valor está disponível para saque nesta máquina.')
-                break
-            else:
-                print('Este valor não está disponível para saque nesta máquina.')
+          if valor.count(',') >= 1:
+              print('Digite o valor sem vírgula ou ponto, por favor.')
+          elif valor.count('.') >= 1:
+              print('Digite o valor sem vírgula ou ponto, por favor.')
+          elif int(valor) < 5:
+              print('O mínimo para saque é R$ 5,00.')
+          else:
+              if int(valor) <= Total:
+                  print('Este valor está disponível para saque nesta máquina.')
+                  break
+              else:
+                  print('Este valor não está disponível para saque nesta máquina.')
 
-    valor = int(valor)
-    valorR = valor
+      valor = int(valor)
+      valorR = valor
 
-    nota100 = 0
-    nota50 = 0
-    nota20 = 0
-    nota10 = 0
-    nota5 = 0
-    nota2 = 0
-    
-    if valor % 100 >= 0 and valor % 100 != 3 and valor % 100 != 1 and valor >= 100:
-          nota100 = valor // 100
-          valor = valor % 100
-          print('{} nota(s) de R$ 100,00'.format(nota100))
+      nota100 = 0
+      nota50 = 0
+      nota20 = 0
+      nota10 = 0
+      nota5 = 0
+      nota2 = 0
+      
+      if valor % 100 >= 0 and valor % 100 != 3 and valor % 100 != 1 and valor >= 100:
+            nota100 = valor // 100
+            valor = valor % 100
+            print('{} nota(s) de R$ 100,00'.format(nota100))
 
-    if valor % 50 >= 0 and valor % 50 != 3 and valor % 50 != 1 and valor >= 50:
-          nota50 = valor // 50
-          valor = valor % 50
-          print('{} nota(s) de R$ 50,00'.format(nota50))
+      if valor % 50 >= 0 and valor % 50 != 3 and valor % 50 != 1 and valor >= 50:
+            nota50 = valor // 50
+            valor = valor % 50
+            print('{} nota(s) de R$ 50,00'.format(nota50))
 
-    if valor % 20 >= 0 and valor % 20 != 3 and valor % 20 != 1 and valor >= 20:
-          nota20 = valor // 20
-          valor = valor % 20
-          print('{} nota(s) de R$ 20,00'.format(nota20))
+      if valor % 20 >= 0 and valor % 20 != 3 and valor % 20 != 1 and valor >= 20:
+            nota20 = valor // 20
+            valor = valor % 20
+            print('{} nota(s) de R$ 20,00'.format(nota20))
 
-    if valor % 10 >= 0 and valor % 10 != 3 and valor % 10 != 1 and valor >= 10:
-          nota10 = valor // 10
-          valor = valor % 10
-          print('{} nota(s) de R$ 10,00'.format(nota10))
+      if valor % 10 >= 0 and valor % 10 != 3 and valor % 10 != 1 and valor >= 10:
+            nota10 = valor // 10
+            valor = valor % 10
+            print('{} nota(s) de R$ 10,00'.format(nota10))
 
-    if valor % 5 >= 0 and valor % 5 != 3 and valor % 5 != 1 and valor >= 5:
-          nota5 = valor // 5
-          valor = valor % 5
-          print('{} nota(s) de R$ 5,00'.format(nota5))
+      if valor % 5 >= 0 and valor % 5 != 3 and valor % 5 != 1 and valor >= 5:
+            nota5 = valor // 5
+            valor = valor % 5
+            print('{} nota(s) de R$ 5,00'.format(nota5))
 
-    elif valor % 2 != 0:
-          nota5 = (valor - (valor % 10) - 5) // 5
-          valor = (valor % 10 + 5)
-          print('{} nota(s) de R$ 5,00'.format(nota5))
+      elif valor % 2 != 0:
+            nota5 = (valor - (valor % 10) - 5) // 5
+            valor = (valor % 10 + 5)
+            print('{} nota(s) de R$ 5,00'.format(nota5))
 
-    if valor % 2 >= 0 and valor % 2 != 3 and valor % 2 != 1 and valor >= 2:
-          nota2 = valor // 2
-          valor = valor % 2
-          print('{} nota(s) de R$ 2,00'.format(nota2))
+      if valor % 2 >= 0 and valor % 2 != 3 and valor % 2 != 1 and valor >= 2:
+            nota2 = valor // 2
+            valor = valor % 2
+            print('{} nota(s) de R$ 2,00'.format(nota2))
 
-    print('TOTAL = R$ {}'.format(valorR))
-    RE = input('\nCONFIRMAR SAQUE(S/N)? ').strip().upper()
-    if RE == 'N':
-        RE = input('CANCELAR(S/N)? ').strip().upper()
-        if RE == 'S':
-            print('PROCESSO FINALIZADO.')
-            break
+      print('TOTAL = R$ {}'.format(valorR))
+      RE = input('\nCONFIRMAR SAQUE(S/N)? ').strip().upper()
+      if RE == 'N':
+          RE = input('CANCELAR(S/N)? ').strip().upper()
+          if RE == 'S':
+              print('PROCESSO FINALIZADO.')
+              break
+      else:
+          print('Processando notas', end='')
+          Total -= valorR
+          for v in range(1, 4):
+              sleep(0.8)
+              print('.', end='')
+          print('\nRetire suas notas.')
+          sleep(2.3)
+          print('PROCESSO FINALIZADO.')
+          print('\nImprimindo extrato',end='')
+          for v in range(1, 4):
+              sleep(0.8)
+              print('.', end='')
+          print('\n')
+          print('EXTRATO BANCO PYTHON')
+          print('Data: {}/{}/{} \nHorário: {}h e {}min' 
+  '\nTotal: R$ {},00'.format(datetime.date.today().day,
+                    datetime.date.today().month, datetime.date.today().year,
+                    datetime.datetime.today().hour,datetime.datetime.today().minute, valorR))
+          break
+
+# interface para criar a conta no banco
+def interface_criar_conta():
+  sair = "S"
+  while (True):
+    nome_banco()
+    sair = str(input("Deseja criar uma conta[S/N]? ")).strip().upper()
+    if (sair == 'N'):
+      break
+
+    nome_completo = str(input("Informe seu nome completo: "))
+    nome_completo = nome_completo.strip().lower().title()
+    if (len(nome_completo) >0):
+      break
     else:
-        print('Processando notas', end='')
-        Total -= valorR
-        for v in range(1, 4):
-            sleep(0.8)
-            print('.', end='')
-        print('\nRetire suas notas.')
-        sleep(2.3)
-        print('PROCESSO FINALIZADO.')
-        print('\nImprimindo extrato',end='')
-        for v in range(1, 4):
-            sleep(0.8)
-            print('.', end='')
-        print('\n')
-        print('EXTRATO BANCO PYTHON')
-        print('Data: {}/{}/{} \nHorário: {}h e {}min' 
-'\nTotal: R$ {},00'.format(datetime.date.today().day,
-                   datetime.date.today().month, datetime.date.today().year,
-                   datetime.datetime.today().hour,datetime.datetime.today().minute, valorR))
+      print("Informe o nome corretamente")
+      continue
+  if (sair == 'N'):
+    encerrar("CRIACAO DE CONTA")
+    return
+
+  while (True):
+    sair = str(input("Continuar com a criacao da conta[S/N]? ")).strip().upper()
+    if (sair == 'N'):
+      break
+
+    senha1 = str(input("Crie uma senha de 8 digitos: "))
+    senha1 = senha1.strip()
+    senha2 = str(input("Repita a senha criada: "))
+    senha2 = senha2.strip()
+    if ((senha1 == senha2) and (len(senha1) > 0) and (len(senha2) > 0) and (len(senha1) == 8)):
+      break
+    else:
+      print("Informe a senha corretamente")
+      continue
+
+  if (sair == 'N'):
+    encerrar("CRIACAO DE CONTA")
+    return
+
+  print(acessar_contas.criar_conta(nome_completo,senha2))
+  conta_criada_com_sucesso()
+
+#interface para acessar a conta no banco
+def interface_acessar_conta():
+  sair = "S"
+  dados_conta = None
+  while (True):
+    nome_banco()
+    sair = str(input("Deseja acessar sua conta[S/N]? ")).strip().upper()
+    if (sair == 'N'):
+      break
+
+    conta = str(input("Informe o numero da sua conta: "))
+    conta = conta.strip().lower().title()
+
+    senha = str(input("Informe sua senha de 8 digitos: "))
+    senha = senha.strip()
+
+    if ((len(conta) >0) and (len(senha) == 8)):
+      dados_conta = acessar_contas.retornar_conta(conta,senha)
+      if (len(dados_conta) != 0):
+        return dados_conta
+      else:
+        continue
+    else:
+      print("Informe os dados corretos")
+      continue
+
+  if (sair == 'N'):
+    return []
+    
+
+def interface_principal():
+  while True:
+    dados_usuario = None
+    nome_banco()
+    opcoes_conta()
+    opcao = str(input("Opcao escolhida: ")).strip()
+    if (opcao == '0'):
+      interface_criar_conta()
+    elif (opcao == '1'):
+      resposta_encerrar2 = False
+      dados_usuario = interface_acessar_conta()
+      while True:
+        opcoes_acesso(dados_usuario[0][0],dados_usuario[0][1])
+        opcao2 = str(input("Opcao escolhida: ")).strip()
+        if (opcao2 == '0'):
+          break
+        elif (opcao2 == '1'):
+          break
+        elif (opcao2 == '2'):
+          break
+        elif (opcao2 == '3'):
+          break
+        elif (opcao2 == '4'):
+          break
+        elif (opcao2 == '5'):
+          resposta_encerrar2 = True
+          break
+        else:
+          continue
+      if resposta_encerrar2:
+        encerrar()
         break
+    else:
+      resposta_encerrar = str(input("Encerrar Sessao[S/N]?")).strip().upper()
+      if (resposta_encerrar == 'S'):
+        encerrar()
+        break
+      else:
+        continue
 
 
+
+
+interface_principal()
