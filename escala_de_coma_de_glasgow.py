456821465780ae7d5da17c96799debd74e2dd151
Matheus Sanches <matheussanchesleme@gmail.com>
2019-11-28 18:51:14 -0300

    Explicação da escala de Glasgow foi adicionada.

---
+++ escala_de_coma_de_glasgow.py
@@ -0,0 +1,41 @@
+# A Escala de Glasgow foi criada em 1974 por dois professores de neurologia da Universidade de Glasgow (Escocia) 
+# para medir o nivel de consciencia após uma lesao cerebral. 
+
+# Sao avaliados tres parâmetros: abertura ocular, resposta verbal e resposta motora
+# Como e a pontuacao?
+
+# Em cada uma das tres categorias (abertura ocular, resposta verbal e resposta motora) 
+# os medicos atribuem uma pontuacao de 1 a 5 para cada nivel de resposta.
+
+# Abertura Ocular
+
+#     Espontânea: 4
+#     Ao estimulo sonoro: 3
+#     Ao estimulo de pressao: 2
+#     Sem resposta: 1
+
+# Resposta Verbal
+
+#     Fala orientada: 5
+#     Fala confusa: 4
+#     Verbaliza palavras soltas: 3
+#     Verbaliza sons (gemidos): 2
+#     Sem resposta: 1
+
+# Resposta Motora
+
+#     Obedece comandos: 6
+#     Localizacao estimulo de pressao: 5
+#     Flexao normal: 4
+#     Flexao anormal: 3
+#     Extensao anormal: 2
+#     Sem resposta: 1
+
+
+
+
+
+
+
+
+
